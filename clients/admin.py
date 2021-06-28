7f6f105885f252c57b487427b2185024938ed703
WellingtonMarinho <wellington.gnr@hotmail.com>
2021-06-28 01:24:08 -0300

    Add search by name in django admin.

--- clients/admin.py
+++ clients/admin.py
@@ -5,4 +5,4 @@ from .models import People
 class PeopleAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'sex', 'age', 'mobile')
     list_display_links = ('name', )
-
+    search_fields = ('name',)
