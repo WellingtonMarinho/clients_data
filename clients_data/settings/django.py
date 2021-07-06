6c4e9dcd814b246409e971c1003a74d668577d31
WellingtonMarinho <wellington.gnr@hotmail.com>
2021-07-06 05:11:49 -0300

    Update

--- clients_data/settings/django.py
+++ clients_data/settings/django.py
@@ -22,12 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'i08#ljkoux@v8+srf%pm*f23sh7x$ok+6$&d3y^*@w+dt7#xob'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
-
+DEBUG = config('DEBUG', default=True, cast=bool)
+ALLOWED_HOSTS= ['127.0.0.1', '.localhost', '.herokuapp.com']
 
 # Application definition
 
