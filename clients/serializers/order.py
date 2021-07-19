1865e968d606732638a5912360c2da46de8f3167
WellingtonMarinho <wellington.gnr@hotmail.com>
2021-07-19 00:36:14 -0300

    Fxi in bug to POST new orders and orderproducts.

--- clients/serializers/order.py
+++ clients/serializers/order.py
@@ -11,67 +11,26 @@ class ProductSerializer(serializers.ModelSerializer):
 
 
 class OrderProductSerializer(WritableNestedModelSerializer, serializers.Serializer):
-    # order = serializers.SerializerMethodField()
-    product = serializers.PrimaryKeyRelatedField(read_only=True)
-    quantity = serializers.IntegerField()
-
-    # def get_order(self):
-
-    # class Meta:
-    #     model = OrderProduct
-    #     fields = ['id', 'product', 'quantity']
-    #     # fields = '__all__'
-    #
-    #     depth = 1
+    class Meta:
+        model = OrderProduct
+        fields = ['product', 'quantity']
 
 
 class OrderSerializer(serializers.ModelSerializer):
-# class OrderSerializer(WritableNestedModelSerializer, serializers.ModelSerializer):
     products = OrderProductSerializer(many=True)
 
     class Meta:
         model = Order
-        # fields = [
-        #     'id',
-        #     'client',
-        #     'created_at',
-        #     'products'
-        # ]
+
         fields = '__all__'
         depth = 0
 
     def create(self, validated_data):
-        print('#-#'*155)
-        # print(validated_data)
-        # print()
-        # print(dir(validated_data))
-
         order_products = validated_data.pop('products', None)
         order = super().create(validated_data)
 
-
-        # print(validated_data)
-        # print()
-        # print(dir(validated_data))
-        # print()
-        # print(order_products)
-        # print(order)
-        # print()
-        # print(dir(order_products))
-        # print()
-
         for product in order_products:
-            print(order_products)
-            print(type(product))
-            print(dir(product))
-            print(dict(product))
-            # print(**product)
-            # product = Product.objects.get(id=product.get('id'))
-            # print(product)
-            # print(dir(product))
-            print('#-#' * 155)
-
-            OrderProduct.objects.get(order=order, **product)
+            OrderProduct.objects.create(order=order, **product)
 
         return order
 
