982f69bf0fe80b5a1811d5d72e1c714c17991382
WellingtonMarinho <wellington.gnr@hotmail.com>
2021-08-01 00:59:50 -0300

    Testing filter by weight range and refactoring the format code.

--- clients/tests/test_views.py
+++ clients/tests/test_views.py
@@ -9,39 +9,50 @@ class ViewsPeopleTest(TestCase):
         self.assertEqual(200, self.client.get('/elastic/').status_code)
 
     def test_return_itens_per_page(self):
-        value = 15
-        response = self.client.get(f'/elastic/?per_page={value}').json()['results']
-        self.assertEqual(value, len(response))
+        per_page = 15
+        response = self.client.get(f'/elastic/?per_page={per_page}').json().get('results')
+        self.assertEqual(per_page, len(response))
 
     def test_max_itens_per_query(self):
-        value = 69
-        response = self.client.get(f'/elastic/?limit_per_query={value}').json()['count']
-        self.assertEqual(value, response)
+        limit_per_query = 69
+        response = self.client.get(f'/elastic/?limit_per_query={limit_per_query}').json().get('count')
+        self.assertEqual(limit_per_query, response)
 
     def test_filter_by_sex(self):
-        per_page = 3 # limit number to run fast test
+        per_page = 5 # limit number to run fast test
         sexs = ['Masculino', 'Feminino']
+
         for sex in sexs:
             with self.subTest():
-                responses = self.client.get(f'/elastic/?sex={sex}&per_page={per_page}').json()['results']
+                responses = self.client.get(
+                    f'/elastic/?sex={sex}&per_page={per_page}'
+                ).json().get('results')
                 for response in responses:
                     self.assertEqual(sex, response.get('sex'))
 
     def test_filter_by_favorite_color(self):
-        per_page = 3
+        per_page = 5
         favorite_colors = ['azul', 'amarelo', 'preto', 'vermelho', 'verde', 'roxo', 'laranja']
+
         for color in favorite_colors:
             with self.subTest():
-                responses = self.client.get(f'/elastic/?favorite_color={color}&per_page={per_page}').json()['results']
+                responses = self.client.get(
+                    f'/elastic/?favorite_color={color}&per_page={per_page}'
+                ).json().get('results')
                 for response in responses:
-                    print(color, response.get('favorite_color'))
                     self.assertEqual(color, response.get('favorite_color'))
 
     def test_filter_by_weight_range(self):
-        per_page = 3
+        per_page = 5
         labels = ['obesity', 'under_weight', 'right_weight', 'overweight' ]
 
-
+        for label in labels:
+            with self.subTest():
+                responses = self.client.get(
+                    f'/elastic/?weight_range={label}&per_page={per_page}'
+                ).json()['results']
+                for response in responses:
+                    self.assertEqual(label, response.get('weight_range'))
 
 
 
