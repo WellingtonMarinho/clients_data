3c0604e05dd826688e7216c1ceebed6c15cea86b
WellingtonMarinho <wellington.gnr@hotmail.com>
2021-08-01 00:48:58 -0300

    test to filter colors.

--- clients/tests/test_views.py
+++ clients/tests/test_views.py
@@ -19,9 +19,35 @@ class ViewsPeopleTest(TestCase):
         self.assertEqual(value, response)
 
     def test_filter_by_sex(self):
+        per_page = 3 # limit number to run fast test
         sexs = ['Masculino', 'Feminino']
         for sex in sexs:
             with self.subTest():
-                responses = self.client.get(f'/elastic/?sex={sex}').json()['results']
+                responses = self.client.get(f'/elastic/?sex={sex}&per_page={per_page}').json()['results']
                 for response in responses:
                     self.assertEqual(sex, response.get('sex'))
+
+    def test_filter_by_favorite_color(self):
+        per_page = 3
+        favorite_colors = ['azul', 'amarelo', 'preto', 'vermelho', 'verde', 'roxo', 'laranja']
+        for color in favorite_colors:
+            with self.subTest():
+                responses = self.client.get(f'/elastic/?favorite_color={color}&per_page={per_page}').json()['results']
+                for response in responses:
+                    print(color, response.get('favorite_color'))
+                    self.assertEqual(color, response.get('favorite_color'))
+
+    def test_filter_by_weight_range(self):
+        per_page = 3
+        labels = ['obesity', 'under_weight', 'right_weight', 'overweight' ]
+
+
+
+
+
+
+
+
+
+
+
