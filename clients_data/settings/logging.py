69eb83152f7a783bba248197fb2277d400b25312
Wellington Marinho <wellington.gnr@hotmail.com>
2021-08-13 22:54:05 -0300

    Add module to logging in project.

---
+++ clients_data/settings/logging.py
@@ -0,0 +1,14 @@
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+}
