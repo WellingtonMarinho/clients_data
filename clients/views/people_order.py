ba5bd9ae74edaa85e7449c359574dc1ef3d5646d
Wellington Soares <wellington.soares@bomvalor.com.br>
2021-12-23 01:00:49 -0300

    Work in progress.

--- clients/views/people_order.py
+++ clients/views/people_order.py
@@ -2,12 +2,11 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from clients.models import People
-from clients.serializers import PeopleGetSerializer, PeopleOrderSerializer
+from clients.serializers import PeopleOrderSerializer
 
 
 class PeopleOrderAPIView(APIView):
-    def get(self, people_id=10):
+    def get(self, request, people_id):
         people = People.objects.get(id=people_id)
         serializer = PeopleOrderSerializer(people)
-
         return Response(serializer.data, status=status.HTTP_200_OK)
